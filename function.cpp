6181f992cd35c44ef4fb778ee8111c55d04ce52e
nishu65 <nishubhaiqwerty@gmail.com>
2023-08-17 22:18:15 +0530

    c

--- function.cpp
+++ function.cpp
@@ -7,10 +7,10 @@ int sum(int a,int b){
 int main()
 {
     int num1,num2;
-    cout<<"hello world"<<endl66;
-    cout<<"enter 1st number"<<endl;
+    cout<<"hello world"<<endl;
+    cout<<"enter 1st number";
     cin>>num1; 
-    cout<<"enter 2nd number"<<endl;
+    cout<<"enter 2nd number";
     cin>>num2;
     cout<<"sum of num 1 and num 2 is ="<<sum(num1,num2); 
     return 0;
