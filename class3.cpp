09f38c00ddde17526b82179143fb69a42f3183f5
nishu65 <nishubhaiqwerty@gmail.com>
2023-08-26 18:26:34 +0530

    N

---
+++ class3.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class sol{
+public:
+void print(int x[] ){
+    cout<<"array : "<<x[];
+}
+};
+
+int main() {
+int a;
+cout<<"ENTER ARRAY LENGTH : ";
+cin>>a;
+while(a--){
+    int c;
+    cout<<"enter value :";
+    cin>>c;
+    int b[c];
+    for(int i=0;i<c,i++){
+        cout<<"enter value at "<<b[i];
+        cin>>b[i];
+    }
+    sol num;
+    for(int v=0,v<c;v++){
+        
+    }
+
+
+}
+
+
+};
---
+++ class5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void print (int x[]){
+    cout<<x[];
+}
+int main(){
+    int a;
+    int b
+
+}
