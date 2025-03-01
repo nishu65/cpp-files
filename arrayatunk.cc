41b8c6d9fad527513dbfb1ba1ad877f47340f83c
nishu65 <nishubhaiqwerty@gmail.com>
2025-03-01 21:52:03 +0530

    array

---
+++ arrayatunk.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+int main(){
+    // int a[5]={5,2,3,4,5};
+   
+    // a[50]=55;
+
+    // for(int i=0;i<100;i++){
+    //     std::cout<<a[i]<<std::endl;
+    // }    
+
+    std::vector<int> v;
+//     v.push_back(5);
+//     v.push_back(2);
+//     v.push_back(3);
+//     v.push_back(4);
+//    // std::cout<<v[10]<<std::endl;
+//     v[50]=55;
+//     for(int i=0;i<100;i++){
+//         std::cout<<v[i]<<std::endl;
+//     }
+std::cout<<v[55]<<std::endl;
+
+    return 0;
+}
